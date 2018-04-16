3cf1cd912597feb0ddd04944f63d5ad34d49dc0e
Satwik <satwik@IN-BLR-WKS320.local>
2018-04-16 22:28:15 +0530

    api testing with real time

--- test/test.js
+++ test/test.js
@@ -2,21 +2,39 @@ var should = require("should");
 var request = require("request");
 var expect = require("chai").expect;
 var baseUrl = "https://jsonplaceholder.typicode.com";
+var slugUrl = "/posts/";
 var util = require("util");
-
+var postnumber = 1;
 describe('returns true', function() {
     it('returns true', function(done) {
-        request.get({ url: baseUrl + '/posts/1/' },
+        request.get({ url: baseUrl + slugUrl},
             function(error, response, body) {
             		var bodyObj = JSON.parse(body);
-            		expect(bodyObj.userId).to.equal(1);
-            		expect(bodyObj.title).to.equal("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+            		//expect(bodyObj.userId).to.equal(1);
+            		//expect(bodyObj.title).to.equal("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
                     expect(response.statusCode).to.equal(200);
                     console.log(body);
                 done();
             });
     });
  });
+
+
+describe('returns true', function() {
+    it('returns true', function(done) {
+        request.get({ url: baseUrl + slugUrl + postnumber},
+            function(error, response, body) {
+                    var bodyObj = JSON.parse(body);
+                    expect(bodyObj.userId).to.equal(1);
+                    expect(bodyObj.title).to.equal("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+                    expect(response.statusCode).to.equal(200);
+                    console.log(body);
+                done();
+            });
+    });
+ });
+
+
 describe('returns true for comments', function() {
     it('returns true for comments', function(done) {
          request.get({ url: baseUrl + '/comments/4/' },
